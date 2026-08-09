2dd4c0524ca790a6599f1d6717be0dfd17799de4
fazlerabbi13 <mohammad0132794@gmail.com>
2026-08-09 12:19:09 +0600

    initialized project with react-router, tailwind and daisy ui.

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+function App() {
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const user = { name, email };
+    console.log(user);
+
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+  return (
+    <>
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl mx-auto mt-60">
+        <div className="card-body">
+          <form onSubmit={handleAddUser}>
+            <fieldset className="fieldset">
+              <label className="label">Name</label>
+              <input
+                type="text"
+                name="name"
+                className="input"
+                placeholder="Name"
+              />
+              <label className="label">Email</label>
+              <input
+                type="email"
+                name="email"
+                className="input"
+                placeholder="Email"
+              />
+              <button className="btn btn-neutral mt-4">Add</button>
+            </fieldset>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
